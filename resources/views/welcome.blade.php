9ef73c0172059a2ea8f7f954b3d03d0c30122b94
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-02 15:50:20 +0300

    Add laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <button class="btn btn-primary">Click me</button>
+@endsection
