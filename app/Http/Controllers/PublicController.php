17e8e0ab5df2317d306b19ea664a0a72f58ac8a0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-03 11:24:47 +0300

    Add Articles migration, seeder, factory

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $articles = Article::all();
+        return view('welcome', compact('articles'));
     }
 
     public function about(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,22 @@
-@extends('layout')
+@extends('partials.layout')
 
 @section('content')
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    <button class="btn btn-primary">Click me</button>
+    <div class="container mx-auto">
+        <div class="flex flex-row flex-wrap">
+            @foreach($articles as $article)
+                <div class="basis-1/4 mb-4">
+                    <div class="card w-96 bg-base-100 shadow-xl h-full">
+{{--                        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>--}}
+                        <div class="card-body">
+                            <h2 class="card-title">{{ $article->title }}</h2>
+                            <p>{{ $article->body }}</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
