fbaf7fed017a06f5079e602ec653b34f54f01bf3
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-17 09:11:07 +0300

    Improve register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,8 +23,8 @@
                     <label class="label" for="email">
                         <span class="label-text">{{__('Email')}}</span>
                     </label>
+                    <input id="email" name="email" type="email" placeholder="Email" value="{{old('email')}}"  required autocomplete="username" class="input input-bordered w-full @error('name') input-error @enderror"/>
 
-                    <x-text-input id="email" class="input input-bordered w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
@@ -34,12 +34,7 @@
                         <span class="label-text">{{__('Password')}}</span>
                     </label>
 
-
-                    <x-text-input id="password" class="input input-bordered w-full"
-                                    type="password"
-                                    name="password"
-                                    required autocomplete="new-password" />
-
+                    <input id="password" name="password" type="password"   required autocomplete="new-password" class="input input-bordered w-full @error('name') input-error @enderror"/>
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
 
@@ -48,22 +43,18 @@
                     <label class="label" for="password_confirmation">
                         <span class="label-text">{{__('Confirm Password')}}</span>
                     </label>
-
-                    <x-text-input id="password_confirmation" class="input input-bordered w-full"
-                                    type="password"
-                                    name="password_confirmation" required autocomplete="new-password" />
+                    <input id="password_confirmation" name="password_confirmation" type="password"   required autocomplete="new-password" class="input input-bordered w-full @error('name') input-error @enderror"/>
 
                     <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                 </div>
 
                 <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                    <a class="btn btn-link" href="{{ route('login') }}">
                         {{ __('Already registered?') }}
                     </a>
 
-                    <x-primary-button class="ml-4">
-                        {{ __('Register') }}
-                    </x-primary-button>
+                    <input type="submit" class="btn btn-primary ml-3" value="{{ __('Register') }}">
+
                 </div>
             </form>
             </div>
